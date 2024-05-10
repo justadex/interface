e7c0fc3a3e213e535986f644f17f04c07ad07b29
Rohan Reddy <rohanreddy888@gmail.com>
2024-05-10 20:40:41 +0530

    Working ReadContract

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -2,6 +2,7 @@
 import { useReadContract } from "wagmi";
 import { YakRouterABI } from "./abi/YakRouterABI";
 import { formatEther } from "viem";
+import { ethers } from "ethers";
 
 export default function Page() {
   const result = useReadContract({
@@ -9,14 +10,33 @@ export default function Page() {
     address: "0x64f1Cd91F37553E5A8718f7D235e5078C962b7e7",
     functionName: "findBestPathWithGas",
     args: [
-      BigInt(formatEther(BigInt("1000000000000000000"), "wei")),
+      BigInt("1000000000000000000"),
       "0x4200000000000000000000000000000000000006",
       "0xf0F161fDA2712DB8b566946122a5af183995e2eD",
-      BigInt(4),
-      BigInt(formatEther(BigInt("1000000000000000000"), "wei")),
+      BigInt("4"),
+      BigInt("1000000000000000000"),
     ],
   });
-  console.log(result.data);
+  console.log(result);
+  //   async function getBestPaths() {
+  //     const provider = new ethers.providers.JsonRpcProvider(
+  //       "https://mainnet.mode.network"
+  //     );
+  //     const yakRouter = new ethers.Contract(
+  //       "0x64f1Cd91F37553E5A8718f7D235e5078C962b7e7",
+  //       YakRouterABI,
+  //       provider
+  //     );
+  //     const result = await yakRouter.findBestPathWithGas(
+  //       ethers.BigNumber.from("1000000000000000000"), // 1 ETH in wei
+  //       "0x4200000000000000000000000000000000000006",
+  //       "0xf0F161fDA2712DB8b566946122a5af183995e2eD",
+  //       ethers.BigNumber.from(4),
+  //       ethers.BigNumber.from("1000000000000000000") // 1 ETH in wei
+  //     );
+  //     console.log(result);
+  //   }
+  //   getBestPaths();
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
       <div>App</div>
