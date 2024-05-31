e4418f395e8d8398dbe103c630bc127982598e9a
Ankith <ankith@Ankith--MacBook-Air.local>
2024-06-01 00:42:08 +0530

    merged changes

--- src/app/app/main.tsx
+++ src/app/app/main.tsx
@@ -206,7 +206,7 @@ const Swap = () => {
   }
 
   return (
-    <section className="flex flex-col gap-6 items-center justify-center min-h-screen relative px-8">
+    <section className="flex flex-col gap-6 items-center justify-start min-h-screen relative px-8 pt-44">
       {address && <PointsTracker walletAddress={address} />}
       <div className="w-full max-w-lg p-4 rounded-2xl shadow-sm bg-primary border-[1px] border-white/20 text-offwhite">
         <div className="flex flex-row items-center justify-between gap-4">
